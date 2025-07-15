36755c3120efbf316833ef0ec9427f5f730b3fda
beanburger <sz215@cantab.net>
2025-07-15 01:31:20 +0100

    complete stat generation and mmseqs alignment.

--- src/mmseqs2.py
+++ src/mmseqs2.py
@@ -7,60 +7,55 @@ from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 
 
-def write_pdb_per_chain_to_fasta(fp_pdb: str, is_cif, rp_fasta):
-    if is_cif:
-        parser = MMCIFParser(QUIET=True)
-    else:
-        parser = PDBParser(QUIET=True)
+def relpath_mmseqs_dir(het_hom: str) -> str:
+    rp_mmseqs_dir = os.path.join('..', 'data', 'NMR', 'mmseqs', het_hom)
+    return rp_mmseqs_dir
 
-    structure = parser.get_structure('structure', fp_pdb)
-    model = structure[0] # I use model 0
-    ppb = PPBuilder() # Init polypeptide builder
-    chain_sequences = {}
-    records = []
 
-    for chain in model:  # Loop over all chains
-        chain_id = chain.id
+def write_fasta(het_hom: str, pdbid: str, pdbid_chains_dict: dict, bio_struct) -> str:
+    rp_mmseqs_dir = relpath_mmseqs_dir(het_hom=het_hom)
+    rp_fasta_dir = os.path.join(rp_mmseqs_dir, 'fasta')
+    os.makedirs(rp_fasta_dir, exist_ok=True)
 
-        # Some chains may have missing residues
-        peptides = ppb.build_peptides(chain)
-        if not peptides:
+    model = bio_struct[0] # Using first model only
+    ppb = PPBuilder()
+    chain_sequences, records = {}, []
+    for chain in model:
+        if chain.id not in pdbid_chains_dict[pdbid]:
             continue
-
-        # Concatenate all fragments for this chain
-        seq = ''.join(str(pp.get_sequence()) for pp in peptides)
-        chain_sequences[chain_id] = seq
-        # Create SeqRecord:
-        record = SeqRecord(
-            Seq(seq),
-            id=f'{os.path.basename(fp_pdb)}_{chain_id}',
-            description=''
-        )
-        records.append(record)
-
-    SeqIO.write(records, rp_fasta, 'fasta')  # Write FASTA
-    return chain_sequences
+        else:
+            peptides = ppb.build_peptides(chain)
+            seq = ''.join(str(pp.get_sequence()) for pp in peptides)
+            chain_sequences[chain.id] = seq
+            record = SeqRecord(Seq(seq), id=f'{pdbid}_{chain.id}', description='')
+            records.append(record)
+    rp_fasta_file = os.path.join(rp_fasta_dir, f'{pdbid}_seqs.fasta')
+    SeqIO.write(records, rp_fasta_file, 'fasta')
+    return rp_fasta_file
 
 
-def run_mmseqs_all_vs_all(rp_fasta, output_prefix: str, output_dir2delete: str):
+def run_mmseqs_all_vs_all(rp_fasta_f, pdbid: str):
     """
     Runs MMseqs2 all-vs-all search on the provided FASTA file.
     Outputs:
       - results.m8 (tabular results)
       - Pandas DataFrame of alignments
     """
-    shutil.rmtree(output_dir2delete, ignore_errors=True)
-    # Working subdirectories and files
-    tmp_dir = os.path.join(output_dir2delete, f'{output_prefix}_tmp')
+    rp_mmseqs_dir = relpath_mmseqs_dir(het_hom=het_hom)
+    rp_dir2delete = os.path.join(rp_mmseqs_dir, 'dir2delete')
+    shutil.rmtree(rp_dir2delete, ignore_errors=True)
+
+    os.makedirs(rp_dir2delete, exist_ok=True)
+    tmp_dir = os.path.join(rp_dir2delete, f'{pdbid}_tmp')
     os.makedirs(tmp_dir, exist_ok=True)
 
-    db_name = os.path.join(output_dir2delete, f'{output_prefix}_db')
-    result_db = os.path.join(output_dir2delete, f'{output_prefix}_result')
-    output_m8 = os.path.join(output_dir2delete, f'{output_prefix}_results.m8')
+    db_name = os.path.join(rp_dir2delete, f'{pdbid}_db')
+    result_db = os.path.join(rp_dir2delete, f'{pdbid}_result')
+    output_m8 = os.path.join(rp_dir2delete, f'{pdbid}_results.m8')
 
     # 1. Create database:
     subprocess.run(['mmseqs', 'createdb',
-                    rp_fasta, db_name],
+                    rp_fasta_f, db_name],
                    stdout=subprocess.DEVNULL, check=True)
 
     # 2. Search: all-vs-all:
@@ -70,6 +65,7 @@ def run_mmseqs_all_vs_all(rp_fasta, output_prefix: str, output_dir2delete: str):
     #     result_db, tmp_dir, '--threads', '4'],
     #                stdout=subprocess.DEVNULL, check=True)
 
+    # 2. Search: all-vs-all (with lower threshold):
     subprocess.run(['mmseqs', 'search',
                     db_name, db_name,
                     result_db, tmp_dir,
@@ -78,9 +74,6 @@ def run_mmseqs_all_vs_all(rp_fasta, output_prefix: str, output_dir2delete: str):
                     '-s', '7.5',
                     '-e', '1000'],
                    stdout=subprocess.DEVNULL, check=True)
-
-
-
 
     # 3. Convert alignments to tabular output
     subprocess.run(['mmseqs', 'convertalis',
@@ -95,25 +88,64 @@ def run_mmseqs_all_vs_all(rp_fasta, output_prefix: str, output_dir2delete: str):
     return df
 
 
-if __name__ == '__main__':
-    pdbid = '1A0N'
-    _meric = 'heteromeric'
-    relpath_pdb = f'../data/NMR/raw_cifs/{_meric}/{pdbid}.cif'  # or '/1A0N.pdb'
-    mmseqs_dir = '../data/NMR/mmseqs'
-    fasta_dir = os.path.join(mmseqs_dir, 'fasta')
-    os.makedirs(fasta_dir, exist_ok=True)
-    relpath_fasta = os.path.join(fasta_dir, f'{pdbid}_seqs.fasta')
-    seqs = write_pdb_per_chain_to_fasta(fp_pdb=relpath_pdb, is_cif=relpath_pdb.endswith('.cif'), rp_fasta=relpath_fasta)
-    print('Extracted sequences:')
-    for chain, sequence in seqs.items():
-        print(f'Chain {chain}: {sequence}')
+def filter_results(pdf):
+    pdf = pdf[pdf['query'] != pdf['target']]
 
-    output_dir2delete = os.path.join(mmseqs_dir, 'dir2delete')
-    os.makedirs(output_dir2delete, exist_ok=True)
-    output_dir2delete = os.path.join(output_dir2delete, pdbid)
-    df_results = run_mmseqs_all_vs_all(rp_fasta=relpath_fasta, output_prefix=_meric, output_dir2delete=output_dir2delete)
+    rp_mmseqs_dir = relpath_mmseqs_dir(het_hom)
+
+    if not pdf.empty:
+        pdf_csv_results_dir = os.path.join(rp_mmseqs_dir, 'results')
+        os.makedirs(pdf_csv_results_dir, exist_ok=True)
+        df_results.to_csv(os.path.join(pdf_csv_results_dir, f'{pdbid}.csv'), index=False)
+    else:
+        print(f'No results for {pdbid}. Adding id to list file.')
+        rp_zero_idty_lst = os.path.join(rp_mmseqs_dir, 'PDBid_no_idty.lst')
+
+        if os.path.exists(rp_zero_idty_lst):
+            with open(rp_zero_idty_lst, 'r') as f:
+                pdbids_no_idty = f.readlines()
+            pdbids_no_idty = [_pdbid.removesuffix('\n') for _pdbid in pdbids_no_idty]
+            pdbids_no_idty.append(pdbid)
+            pdbids_no_idty.sort()
+            with open(rp_zero_idty_lst, 'w') as f:
+                f.write('\n'.join(pdbids_no_idty) + '\n')
+        else:
+            with open(rp_zero_idty_lst, 'w') as f:
+                f.write(f'{pdbid}\n')
+    return pdf
+
+
+if __name__ == '__main__':
+    het_hom = 'heteromeric'
+    pdbid = '1A0N'
+    rp_cif = os.path.join('..', 'data', 'NMR', 'raw_cifs', het_hom, f'{pdbid}.cif')
+    parser = MMCIFParser(QUIET=True)
+    bio_struct = parser.get_structure('', rp_cif)
+    rp_fasta_file = write_fasta(het_hom, pdbid, pdbid_chains_dict={'1A0N': ['A', 'B']}, bio_struct=bio_struct)
+    df_results = run_mmseqs_all_vs_all(rp_fasta_f=rp_fasta_file, pdbid=pdbid)
     print(df_results)
-    pdf_csv_results_dir = os.path.join(mmseqs_dir, 'results')
-    os.makedirs(pdf_csv_results_dir, exist_ok=True)
-    df_results.to_csv(os.path.join(pdf_csv_results_dir, f'{pdbid}.csv'), index=False)
+    filter_results(df_results)
+    # df_results = df_results[df_results['query'] != df_results['target']]
+    #
+    # _rp_mmseqs_dir = relpath_mmseqs_dir(het_hom)
+    # if not df_results.empty:
+    #     _pdf_csv_results_dir = os.path.join(_rp_mmseqs_dir, 'results')
+    #     os.makedirs(_pdf_csv_results_dir, exist_ok=True)
+    #     df_results.to_csv(os.path.join(_pdf_csv_results_dir, f'{pdbid}.csv'), index=False)
+    # else:
+    #     print(f'No results for {pdbid}. Adding id to list file.')
+    #     _rp_zero_idty_lst = os.path.join(_rp_mmseqs_dir, 'PDBid_no_idty.lst')
+    #
+    #     if os.path.exists(_rp_zero_idty_lst):
+    #         with open(_rp_zero_idty_lst, 'r') as f:
+    #             _pdbids_no_idty = f.readlines()
+    #         _pdbids_no_idty = [_pdbid.removesuffix('\n') for _pdbid in _pdbids_no_idty]
+    #         _pdbids_no_idty.append(pdbid)
+    #         _pdbids_no_idty.sort()
+    #         with open(_rp_zero_idty_lst, 'w') as f:
+    #             f.write('\n'.join(_pdbids_no_idty) + '\n')
+    #     else:
+    #         with open(_rp_zero_idty_lst, 'w') as f:
+    #             f.write(f'{pdbid}\n')
+
     pass
--- src/pdb_model_stats.py
+++ src/pdb_model_stats.py
@@ -1,68 +1,91 @@
 import os, glob
 from time import time
+from collections import defaultdict
 import pandas as pd
-from Bio.PDB import MMCIFParser, MMCIF2Dict
-from Bio.PDB.Polypeptide import is_aa
-parser = MMCIFParser(QUIET=True)
+from Bio.PDB import MMCIFParser, PDBParser, MMCIF2Dict
 import mmseqs2
 
-def generate_stats(rp_pdbid_chains: str, meric: str):
+def generate_stats(rp_pdbid_chains: str=None, use_mmcif=True):
+    """
+    After parsing raw mmCIFs, the resulting PDBid_chains were written to a .lst file, which is used as follows:
+
+    1. Read raw mmCIFs via Bio.PDB.MMCIFParser for each PDBid in .lst to get:
+        - deposition year for each PDBid
+        - total number of chains for each PDBid.
+
+    2. Read PDBid_chains .lst file to get:
+        - number of protein chains per PDBid.
+
+    3. Read parsed SSV files to dataframes for each PDBid_chain in .lst to get:
+        - number of models per PDBid_chain.
+        - number of alpha carbons per PDBid_chain.
+
+    4. Calculate sequence identity between every chain for each PDBid from list, by MMseqs2.
+    """
     start = time()
-    parser = MMCIFParser(QUIET=True)
-    data = []
+    if not use_mmcif:
+        parser = PDBParser(QUIET=True)
+    else:
+        parser = MMCIFParser(QUIET=True)
+    stats, pdbids = [], []
+
+    if not rp_pdbid_chains:
+        rp_pdbid_chains = os.path.join('..', 'data', 'NMR', 'multimodel_PDBids', 'het_multimod_2104_pdbid_chains.txt')
+    het_hom = os.path.basename(rp_pdbid_chains)[:3]
+    het_hom = 'heteromeric' if het_hom == 'het' else 'homomeric'
+
     with open(rp_pdbid_chains, 'r') as f:
         pdbid_chains = [line.rstrip('\n') for line in f]
-    pdbid_chains.sort()
+    pdbid_chains.sort()  # expected to already be sorted (i.e. before writing out), but to avoid relying on other code.
+    pdbid_chains_dict = defaultdict(list)
+
     for pdbid_chain in pdbid_chains:
         pdbid, chain = pdbid_chain.split('_')
-        rpath_cif = f'../data/NMR/raw_cifs/{meric}/{pdbid}.cif'
-        structure = parser.get_structure('', rpath_cif)
-        num_models = len(list(structure))
-        year = structure.header['deposition_date'][:4]
+        pdbids.append(pdbid)
+        pdbid_chains_dict[pdbid].append(chain)
+    # 2104 heteromeric pdbid_chains --> 932 heteromeric pdbids.
+    pdbids = list(set(pdbids))
+    pdbids.sort()
+    pdbid_chains_dict = dict(pdbid_chains_dict)
 
-        # cif_dict = MMCIF2Dict.MMCIF2Dict(rpath_cif)  # a few return '?', so I use 'get_structure()' despite some differences.
-        # year2 = cif_dict.get('_citation.year', ['NA'])[0]
-        # print(f'deposition={year}; citation={year2}')
+    rp_raw_cif_dir = os.path.join('..', 'data', 'NMR', 'raw_cifs', het_hom)
+    rp_token_cif_dir = os.path.join('..', 'data', 'NMR', 'tokenised_cifs', het_hom)
+    for pdbid in pdbids:
+        rp_cif = os.path.join(rp_raw_cif_dir, f'{pdbid}.cif')
+        bio_struct = parser.get_structure('', rp_cif)
+        total_chain_count = len(list(next(bio_struct.get_models()).get_chains()))
+        year = bio_struct.header['deposition_date'][:4]
 
-        chains_set = set()
-        protein_chains_set = set()
+        rp_fasta_file = mmseqs2.write_fasta(het_hom, pdbid, pdbid_chains_dict, bio_struct)
+        result_pdf = mmseqs2.run_mmseqs_all_vs_all(rp_fasta_file, pdbid)
+        filtered_pdf = mmseqs2.filter_results(result_pdf)
+        identity = 0
+        if not filtered_pdf.empty:
+            pass
+            # TODO generate dict of chain key to non-zero identity pairings as value.
 
-        first_model = next(structure.get_models())  # assumes first model can represent all models for this pdbid_chain
-        for chain in first_model:
-            chains_set.add(chain.id)
-            is_protein = False
-            for residue in chain:
-                if is_aa(residue, standard=True):
-                    # print(f'Chain {chain.id}, Residue {residue.get_resname()} is a standard amino acid')
-                    is_protein = True
-                    break
-            if is_protein:
-                protein_chains_set.add(chain.id)
-
-        for chain in first_model:
-            ca_count = sum(1 for atom in chain.get_atoms() if atom.get_name() == 'CA')
-
-            if year != 'NA' and year != '?':
-                print(f'year is given as {year}. Replacing with 0')
-                year = int(year)
-            else:
-                year = 0
-
-            chainpairs_mmseqs_ = mmseqs2.calc_mmseq(pdbid, chain)
-
-            data.append({
-                'PDBid': os.path.basename(rpath_cif).removesuffix('.cif'),
-                'het_hom': os.path.basename(os.path.dirname(rpath_cif))[:3],
+        for chain in pdbid_chains_dict[pdbid]:
+            pdbid_chain = f'{pdbid}_{chain}'
+            rp_tok_cif = os.path.join(rp_token_cif_dir, f'{pdbid_chain}.ssv')
+            pdbid_chain_pdf = pd.read_csv(rp_tok_cif, sep=' ')
+            model_count = len(pdbid_chain_pdf['model'].unique())
+            ca_count = pdbid_chain_pdf.shape[0]
+            stats.append({
+                'PDBid': pdbid,
+                'het_hom': het_hom,
                 'year': year,
-                '#model': num_models,
-                '#chain': int(len(chains_set)),
-                '#prot_chain': int(len(protein_chains_set)),
-                'chain': chain.id,
-                'CA_count': ca_count,
-                'identity': 89,  # TODO
-                'similarity': 100  # TODO
+                '#model': model_count,
+                '#chains': total_chain_count,
+                '#protChains': len(pdbid_chains_dict[pdbid]),
+                'chain': chain,
+                '#alphaCarbs': ca_count,
+                'identity': identity,
+                # 'similarity': 100  # TODO
             })
-    pdf = pd.DataFrame(data)
+    pdf = pd.DataFrame(stats)
+
+
+
     pdf_sorted = pdf.sort_values(by=['year', '#model'], ascending=[True, True])
     print(pdf_sorted.dtypes)
     for col_to_cast in ['PDBid', 'chain']:
@@ -73,10 +96,8 @@ def generate_stats(rp_pdbid_chains: str, meric: str):
 
 
 if __name__ == '__main__':
-    _meric = 'heteromeric'
-    rpath_pdbid_chains = (f'../data/NMR/multimodel_PDBids/het_multimod_2104_pdbid_chains.txt')
-    generate_stats(rp_pdbid_chains=rpath_pdbid_chains, meric=_meric)
-    # pdf = generate_stats(rp_pdbid_chains=rpath_pdbid_chains, meric=_meric)
+    # rpath_pdbid_chains = (f'../data/NMR/multimodel_PDBids/het_multimod_2104_pdbid_chains.txt')
+    stats_pdf = generate_stats()
 
     # # Columns to blank out when duplicated:
     # cols_to_blank = ['PDBid', 'het_hom', 'year', '#model', '#chain', '#prot_chain', 'identity', 'similarity']
@@ -94,3 +115,20 @@ if __name__ == '__main__':
     # pdf.to_csv(f'{csv_dst_dir}/from_{len(cifs)}_cifs.csv', index=False)
 
     pass
+
+    # Note:
+    # cif_dict = MMCIF2Dict.MMCIF2Dict(rpath_cif)
+    # A few of these return '?', so using 'get_structure()' instead,
+    # which broadly same but sometimes differs by as much as 3 years.
+    # year = cif_dict.get('_citation.year', ['NA'])[0]
+    # if year != 'NA' and year != '?':
+    #     print(f'year is given as {year}. Replacing with 0')
+    #     year = int(year)
+    # else:
+    #     year = 0
+
+    # from Bio.PDB.Polypeptide import is_aa
+    # first_model = next(structure.get_models())
+    # for chain in first_model:
+    #     ca_count = sum(1 for atom in chain.get_atoms() if atom.get_name() == 'CA')
+    # num_models = len(list(structure))
