eec8424356f31610aaa002d740de2afad357d1e4
zibaee-2021 <sz215@cantab.net>
2025-08-13 18:55:50 +0100

    Prune pidc stats csv according to specs for dataset v1.0.

--- src/prune_dataset.py
+++ src/prune_dataset.py
@@ -34,20 +34,54 @@ HOWEVER, FOR 'DATASET 1.0', I WILL NOT PRUNE ACCORDING TO THIS.
 
 """
 import os
+import pandas as pd
 
 if __name__ == "__main__":
+
+    # - INCLUDES ONLY PDB-CHAINS WITH 3 OR MORE RESIDUES:
+
+    _1_AA_pidc = ['1GAC_C', '1GAC_D', '2AIZ_B']
+    _2_AA_pidc = ['1GAC_A', '1GAC_B', '1WCO_A', '2K1Q_B', '2M9P_B', '2M9Q_B', '2MX6_B', '3CYS_B']
+    _3_AA_pidc = ['1CFA_B', '2KID_B', '2RUI_B']
+
     with open(os.path.join('..','data','NMR','multimodel_lists', 'multimod_2713_hetallchains_hom1chain.lst'), 'r') as f:
-        pidc_2713 = f.read().splitlines()
+        pidc_2713 = sorted(f.read().splitlines())
+    rp_datasets_lists_dir = os.path.join('..','data','NMR', 'datasets', 'lists_specs')
+    os.makedirs(rp_datasets_lists_dir, exist_ok=True)
+    rp_dataset_1p0_lst = os.path.join(rp_datasets_lists_dir, '2702_pidc.lst')
 
-    pidc_2713.sort()
+    pidc_2702 = [pidc for pidc in pidc_2713 if pidc not in _1_AA_pidc + _2_AA_pidc]
+    with open(rp_dataset_1p0_lst, 'w') as f:
+        f.write('\n'.join(pidc_2702))
 
-    rp_datasets_dir = os.path.join('..','data','NMR', 'datasets')
-    os.makedirs(rp_datasets_dir, exist_ok=True)
-    pidc_2702 = []
-    for pidc in pidc_2713:
-        if pidc in ['2AIZ_B', '1GAC_D', '1GAC_C', '1GAC_A', '1GAC_B', '1WCO_A',
-                   '2K1Q_B', '2M9P_B', '2M9Q_B', '2MX6_B', '3CYS_B']:
-            continue
-        else:
-            pidc_2702.append(pidc)
+ # - EXCLUDES ANY THAT HAVE "EXTREME RMSD" AND "EXTREME TM-SCORE":
+
+    rp_mm_2713_pidc = os.path.join('..', 'data', 'NMR', 'stats', 'multimod_2713_hetallchains_hom1chain', 'mm_2713_pidc.csv')
+    mm_2713_pdf = pd.read_csv(rp_mm_2713_pidc)
+
+    # 1. Apply size filter:
+    mask_pidc = mm_2713_pdf['CA_count'] < 3
+    # print(mask_pidc.sum())
+    # print(mm_2713_pdf[mask_pidc])
+
+    # 2. Apply structural filters:
+    mask_structural = (
+        (mm_2713_pdf['meanRMSD'] > 10) &
+        (mm_2713_pdf['stdevRMSD'] < 0.01) &
+        (mm_2713_pdf['meanTMS'] < 0.2) &
+        (mm_2713_pdf['stdevTMS'] < 0.1)
+    )
+    # print(mask_structural.sum())
+    # print(mm_2713_pdf[mask_structural])
+    # print(mm_2713_pdf['meanTMS'].describe())
+    # print(mm_2713_pdf['stdevTMS'].describe())
+    # print(mm_2713_pdf['meanRMSD'].describe())
+    # print(mm_2713_pdf['stdevRMSD'].describe())
+
+    # 3. Combine filters:
+    dataset_1p0 = mm_2713_pdf.loc[~mask_pidc]
+    dataset_1p0 = dataset_1p0.loc[~mask_structural]
+    print(dataset_1p0.shape)
+    rp_datasets_dir = os.path.join('..', 'data', 'NMR', 'datasets')
+    dataset_1p0.to_csv(os.path.join(rp_datasets_dir, 'NMR_1p0.csv'))
 
