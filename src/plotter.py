d842de05fd3d64e2a9719d702dd513fbb90e5ff3
zibaee-2021 <sz215@cantab.net>
2025-08-21 16:57:00 +0100

    Add function to call RMSD.py's implementations of dendrogram, heatmap and contourmap.

--- src/plotter.py
+++ src/plotter.py
@@ -1,15 +1,23 @@
-import os
-import math
+import os, glob, math, json
+import orjson
 from collections import Counter
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import squareform
+from scipy.cluster.hierarchy import linkage, fcluster
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 from Bio import SeqIO
+import RMSD
 
 # RELATIVE PATHS:
 def _rp_nmr_dir() -> str:
     return os.path.join('..', 'data', 'NMR')
+
+def _rp_rmsd_dir(sub_dir: str) -> str:
+    return os.path.join(_rp_nmr_dir(), 'RMSD', sub_dir)
 
 def _rp_stats_dir(sub_dir: str) -> str:
     return os.path.join(_rp_nmr_dir(), 'stats', sub_dir)
@@ -50,7 +58,6 @@ def plot_counts(attribute: str, attr_counts: list, bin_size: int=1, num_pidchain
     fig.tight_layout()
     plt.show()
 
-
 def _calc_model_counts(rp_parsed_cifs_ssvs: list) -> list:
     model_counts = list()
     pidc_list, model_count_list = [], []
@@ -69,7 +76,6 @@ def _calc_model_counts(rp_parsed_cifs_ssvs: list) -> list:
     rp_dst_csv = os.path.join(_rp_stats_dir('multimod_2713_hetallchains_hom1chain'), 'model_counts.csv')
     model_counts_pidc_pdf.to_csv(rp_dst_csv, index=False)
     return sorted(model_counts)
-
 
 def _calc_ca_counts(rp_parsed_cifs_ssvs: list) -> list:
     ca_counts = list()
@@ -94,7 +100,6 @@ def _calc_ca_counts(rp_parsed_cifs_ssvs: list) -> list:
     rp_dst_csv = os.path.join(_rp_stats_dir('multimod_2713_hetallchains_hom1chain'), 'ca_counts.csv')
     ca_counts_pidc_pdf.to_csv(rp_dst_csv, index=False)
     return sorted(ca_counts)
-
 
 def plot_fasta_size_distribution(rp_fasta_f, x_limit_220=False):
     seq_lengths = [len(record.seq) for record in SeqIO.parse(rp_fasta_f, 'fasta')]
@@ -144,7 +149,6 @@ def plot_fasta_size_distribution(rp_fasta_f, x_limit_220=False):
     plt.title('Distribution of PDB Sequence Lengths', fontsize=12)
     fig.tight_layout()
     plt.show()
-
 
 def violin_plot(pdf):
     plt.figure(figsize=(14, 6))
@@ -196,7 +200,6 @@ def plot_rmsds_and_stdev(pdf):
     plt.tight_layout()
     plt.show()
 
-
 def plot_rmsd_and_tms(pdf):
     # sns.set_theme(style='whitegrid', context='talk')
     fig, ax1 = plt.subplots(figsize=(8, 5))
@@ -227,11 +230,44 @@ def plot_rmsd_and_tms(pdf):
     plt.tight_layout()
     plt.show()
 
+def dendrgm_heatmap_contourmap(rp_matrix: str):
+    pidc = os.path.basename(rp_matrix).removesuffix('.npz')
+    loaded = np.load(rp_matrix)
+    rmsd_mat = loaded['mat']
+    n_models = rmsd_mat.shape[0]
+    linkage_mat = RMSD.dendrogrm(rmsd_mat, n_models, pidc)
+    RMSD.heatmap(rmsd_mat, linkage_mat)
+    RMSD.contour_map(rmsd_mat)
+
 
 if __name__ == '__main__':
+    rp_rmsd_mat_dir = os.path.join(_rp_rmsd_dir('multimod_2713_hetallchains_hom1chain'), 'rmsd_matrices')
+    rp_rmsd_mats = sorted(glob.glob(os.path.join(rp_rmsd_mat_dir, '*.npz')))
+    results, res = list(), dict()
+    for rp_rmsd_mat in rp_rmsd_mats[1:2]:
+        pidc = os.path.basename(rp_rmsd_mat).removesuffix('.npz')
+        print(pidc)
+        loaded = np.load(rp_rmsd_mat)
+        rmsd_mat = loaded['mat']
+        dendrgm_heatmap_contourmap(rp_rmsd_mat)
+        threshold = 4.0
+        clusters_dict = RMSD.cluster_models(rmsd_mat, threshold)
+        rp_rmsd_clusters_dir = os.path.join(rp_rmsd_mat_dir, f'clusters_{str(threshold).replace('.', 'p')}')
+        os.makedirs(rp_rmsd_clusters_dir, exist_ok=True)
 
-    rp_stats_pidc_2713_csv = os.path.join('..', 'data', 'NMR', 'stats',
-                                     'multimod_2713_hetallchains_hom1chain', 'mm_2713_pidc.csv')
-    pidc_2713_pdf = pd.read_csv(rp_stats_pidc_2713_csv)
-    pidc_2713_pdf = pidc_2713_pdf.sort_values(by=['meanRMSD'], ascending=False)
-    plot_rmsd_and_tms(pidc_2713_pdf)
+        clusters_dict = {int(k): v for k, v in clusters_dict.items()}
+
+        with open(os.path.join(rp_rmsd_clusters_dir, f'{pidc}.json'), 'w') as f:
+            json.dump(clusters_dict, f)
+
+
+    # rp_rmsd_mat_dir = os.path.join(_rp_rmsd_dir('multimod_2713_hetallchains_hom1chain'), 'rmsd_matrices')
+    # rp_rmsd_mats = sorted(glob.glob(os.path.join(rp_rmsd_mat_dir, '*.npz')))
+    # for rp_rmsd_mat in rp_rmsd_mats[:10]:
+    #     dendrgm_heatmap_contourmap(rp_rmsd_mat)
+    # pass
+    # rp_stats_pidc_2713_csv = os.path.join('..', 'data', 'NMR', 'stats',
+    #                                  'multimod_2713_hetallchains_hom1chain', 'mm_2713_pidc.csv')
+    # pidc_2713_pdf = pd.read_csv(rp_stats_pidc_2713_csv)
+    # pidc_2713_pdf = pidc_2713_pdf.sort_values(by=['meanRMSD'], ascending=False)
+    # plot_rmsd_and_tms(pidc_2713_pdf)
